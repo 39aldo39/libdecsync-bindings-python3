2054df0553ed7a8db8df4dd95ecae84af5e89ed2
Aldo Gunsing <dev@aldogunsing.nl>
2021-10-03 22:21:35 +0200

    Bump version to 2.0.2

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import setup
 
 setup(
     name="libdecsync",
-    version="2.0.0",
+    version="2.0.2",
     author="Aldo Gunsing",
     author_email="dev@aldogunsing.nl",
     url="https://github.com/39aldo39/libdecsync-bindings-python3",
